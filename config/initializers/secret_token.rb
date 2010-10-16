8cbdbe72847bfc9321ff7309fb9af61ee5fa90c6
jduff <duff.john@gmail.com>
2010-10-15 20:57:23 -0400

    configurable secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Todoly::Application.config.secret_token = '1324eea5d90366b07cc4da7c71af4dc2cc6ea8b377b0b6070c40b3cfb6203f71c6d24429878c1bcce8de3cf2e0c8463af47e08fdbeefe902268f8564d2cbfb35'
+Todoly::Application.config.secret_token = ENV['APP_SECRET_TOKEN'] || '1324eea5d90366b07cc4da7c71af4dc2cc6ea8b377b0b6070c40b3cfb6203f71c6d24429878c1bcce8de3cf2e0c8463af47e08fdbeefe902268f8564d2cbfb35'
